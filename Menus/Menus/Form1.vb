71240b4ba1c2d089dcf659d2dd3052d048e4a6f3
Wesley <wrcar213@learner.dvusd.org>
2024-01-09 13:56:16 -0700

    Added Save and Open to menu

--- Menus/Menus/Form1.vb
+++ Menus/Menus/Form1.vb
@@ -6,4 +6,18 @@
     Private Sub CloseToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles CloseToolStripMenuItem.Click
         Me.Close()
     End Sub
+
+    Private Sub SaveToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SaveToolStripMenuItem.Click
+        Dim OutFile As New IO.StreamWriter("out.txt")
+        OutFile.Write(TextBox1.Text)
+        OutFile.Close()
+    End Sub
+
+    Private Sub OpenToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles OpenToolStripMenuItem.Click
+        If IO.File.Exists("out.txt") Then
+            Dim inFile As New IO.StreamReader("out.txt")
+            TextBox1.Text = inFile.ReadToEnd
+            inFile.Close()
+        End If
+    End Sub
 End Class
